886647d1ae0461b5e59cf4ff5f30a8d080ae8b58
SalmaFabel <salma.hernandez16@tectijuana.edu.mx>
2020-10-12 13:26:18 -0700

    Practica 1

--- Practicas/Practica 1.R
+++ Practicas/Practica 1.R
@@ -1,24 +1,34 @@
-#Pruebe la ley de los nÃºmeros grandes para N nÃºmeros aleatorios normalmente distribuidos con media = 0, stdev = 1:
-  
-#Cree un script R que cuente cuÃ¡ntos de estos nÃºmeros se encuentran entre -1 y 1 y divida por la cantidad total de N
 
-#Sabes que E (X) = 68,2%
-#Compruebe que Mean (Xn) -> E (X) mientras vuelve a ejecutar su script mientras aumenta N
+# Pruebe la ley de los números grandes para N números aleatorios normalmente distribuidos con media = 0, stdev = 1:
 
-#InsinuaciÃ³n:
-#1. Inicializar el tamaÃ±o de la muestra
-#2. Inicializar contador
-#3. bucle para (i en rnorm (tamaÃ±o))
-#4. Verifique si la variable iterada cae
-#5. Aumente el contador si la condiciÃ³n es verdadera
-#6. devuelve un resultado <- contador / N
+# Cree un script R que contará cuántos de estos números se encuentran entre -1 y 1 y dividirá
+# por la cantidad total de N
 
-N  <- 1000
-Contador  <- 0
+# Sabes que E (X) = 68,2%
+# Verifique que Mean (Xn) -> E (X) mientras vuelve a ejecutar su script mientras aumenta N
 
-for(i in rnorm( ))
+
+# Pista:
+# 1. Inicializar el tamaño de la muestra
+# 2. Inicializar contador
+# 3. bucle para (i en rnorm (tamaño))
+# 4. Verifique si la variable iterada cae
+# 5. Aumente el contador si la condición es verdadera
+# 6. devuelve un resultado <- contador / N
+
+N <-10000
+Counter <-0
+
+for(i in rnorm(N, mean = 0, sd=1) )
 {
-  if(i >=-1 && i <=1){
-    Counter<- Counter+1
+  if(i >= -1 && i <= 1){
+    Contador<- Contador+1
+  }
+  else
+  {
+    
   }
 }
+resultado <- Contador/N
+
+print(resultado)#[1] 0.6826
