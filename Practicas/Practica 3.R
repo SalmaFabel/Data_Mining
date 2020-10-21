ddb1225c7446a66232c92dc0ea27d4d59ecc902c
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2020-10-20 21:09:16 -0700

    Practice 3  finished

--- Practicas/Practica 3.R
+++ Practicas/Practica 3.R
@@ -1,32 +1,32 @@
-Scenario: You are a Data Scientist working for a consulting firm.
-One of your colleagues from the Auditing Department has asked you
-to help them assess the financial statement of organization X.
+#Scenario: You are a Data Scientist working for a consulting firm.
+#One of your colleagues from the Auditing Department has asked you
+#to help them assess the financial statement of organization X.
 
-You have been supplied with two vector of data: mounthly revenue and
-expenses for the financial year in quiestion. Your task is to calculate
-the following financial matrics:
+#You have been supplied with two vector of data: mounthly revenue and
+#expenses for the financial year in quiestion. Your task is to calculate
+#the following financial matrics:
   
-- profit for each mounth
-- profit after tax for each month (the tax rate is 30%)
-- profit margin for each month - equal to profit after tax divided by revenue
-- good months - where the profit after tax was greater than the mean for the year
-- bad months - where the profit after tax was less then the mean for years
-- the best month - where the profit after tax was max for the year
-- the worst month - where the profit after tax was min for the year
+#- profit for each mounth
+#- profit after tax for each month (the tax rate is 30%)
+#- profit margin for each month - equal to profit after tax divided by revenue
+#- good months - where the profit after tax was greater than the mean for the year
+#- bad months - where the profit after tax was less then the mean for years
+#- the best month - where the profit after tax was max for the year
+#- the worst month - where the profit after tax was min for the year
 
-All results need to be presented as vectors.
+#All results need to be presented as vectors.
 
-Results for dollar values need to be calculate with $0.01 precision, but need to be
-presented in Units of $1,000(i.e. 1k) with no decimal point.
+#Results for dollar values need to be calculate with $0.01 precision, but need to be
+#presented in Units of $1,000(i.e. 1k) with no decimal point.
 
-Results for the profit margin ratio needed to be presented in units of % with no
-decimal points.
+#Results for the profit margin ratio needed to be presented in units of % with no
+#decimal points.
 
-Note: Your collegue has warned you that it is okay for tax for any given month to be
-negative (in accounting terms, negative tax translates into a deferred tax asset).
+#Note: Your collegue has warned you that it is okay for tax for any given month to be
+#negative (in accounting terms, negative tax translates into a deferred tax asset).
 
-Hint 1
-Use:
+#Hint 1
+#Use:
 round()
 mean()
 max()
@@ -42,43 +42,43 @@ profit <- revenue - expenses
 profit
 
 #Calculate Tax As 30% Of Profit And Round To 2 Decimal Points
-tax <- round(0.30 * _, _)
+tax <- round(0.30 * profit, 2)
 tax 
 
 #Calculate Profit Remaining After Tax Is Deducted
-profit.after.tax <- _ - _
+profit.after.tax <- profit - tax
 profit.after.tax
 
 #Calculate The Profit Margin As Profit After Tax Over Revenue
 #Round To 2 Decimal Points, Then Multiply By 100 To Get %
-profit.margin <- _(_ / _, 2) * _
+profit.margin <- round(profit.after.tax/revenue, 2) * 100
 profit.margin
 
 #Calculate The Mean Profit After Tax For The 12 Months
-mean_pat <- _(profit.after.tax)
+mean_pat <- mean(profit.after.tax)
 mean_pat
 
 #Find The Months With Above-Mean Profit After Tax
-good.months <- _ _ mean_pat
+good.months <- profit.after.tax > mean_pat
 good.months
 
 #Bad Months Are The Opposite Of Good Months !
-bad.months <- _good.months
+bad.months <- !good.months
 bad.months
 
 #The Best Month Is Where Profit After Tax Was Equal To The Maximum
-best.month <- profit.after.tax == _(profit.after.tax)
+best.month <- profit.after.tax == max(profit.after.tax)
 best.month
 
 #The Worst Month Is Where Profit After Tax Was Equal To The Minimum
-worst.month <- _ == _(_)
+worst.month <- profit.after.tax == min(profit.after.tax)
 worst.month
 
 #Convert All Calculations To Units Of One Thousand Dollars
-revenue.1000 <- round(revenue / 1000, 0)
-expenses.1000 <- _(_ _ _, _)
-profit.1000 <- _(_ _ _, _)
-profit.after.tax.1000 <- _(_ _ _, _)
+revenue.1000 <- round(revenue/1000,0)
+expenses.1000 <- round(expenses/1000,0)
+profit.1000 <- round(profit/1000,0)
+profit.after.tax.1000 <- round(profit.after.tax/1000,0)
 
 #Print Results
 revenue.1000
@@ -104,5 +104,6 @@ M <- rbind(
   best.month,
   worst.month
 )
+M
 
 #Print The Matrix
