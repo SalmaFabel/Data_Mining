93746f38bdb1b8db61114d98d3ec6c0a1c7abd14
Salma Fabel <salma.hernandez16@tectijuana.edu.mx>
2020-11-22 16:41:04 -0800

    Update ExamU2.R

--- Evaluation/ExamU2.R
+++ Evaluation/ExamU2.R
@@ -6,3 +6,21 @@ setwd("C:\\Users\\salmi\\OneDrive\\Documentos\\GitHub\\Mineria-de-datos\\Evaluat
 
 movies <- read.csv("Project-Data.csv")
 head(movies)
+
+##Correcciones
+movies2<-(movies[,c(3,6,8,18)])
+
+colnames(movies2) <- c("Genre","Studio","Budget", "Gross_US")
+head(movies2)
+
+##
+movies2[movies2$Studio=="Buena Vista Studios",]
+
+##Grafica
+
+p <- ggplot(movies2, aes(x=Genre, y=Gross_US,
+                        color=Studio, size=Budget,
+                        main = "Domestic Gross % by Genre"))
+
+p  + geom_boxplot() 
+
